23c966793068f1d260f18f1e4813e0c9b0e4dec2
Jefferson <jefficostau@gmail.com>
2022-07-20 23:32:08 -0300

    inserção da função de impedir que a bolinha fique presa

--- multiPlayer.js
+++ multiPlayer.js
@@ -55,7 +55,7 @@ function draw() {
   verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);  
   incluiPlacar();
   marcaPonto();
-  //bolinhaNaoFicaPresa();
+  bolinhaNaoFicaPresa();
 }
 
 function mostraBolinha(){
